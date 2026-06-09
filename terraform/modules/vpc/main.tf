8e22ba581efb6874048bab1974b0c4614a2195f4
Ashraf Mahmud <ashraf@filesure.in>
2026-06-10 01:55:05 +0600

    Add Terraform configuration files for EKS platform setup in dev environment

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,83 @@
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+# Public Subnets
+resource "aws_subnet" "public" {
+  for_each = var.public_subnets
+
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = each.value.cidr_block
+  availability_zone       = each.value.az
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = each.value.name
+  }
+}
+# Private Subnet
+resource "aws_subnet" "private" {
+  for_each = var.private_subnets
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = each.value.cidr_block
+  availability_zone  = each.value.az
+
+  tags = {
+    Name = each.value.name
+  }
+}
+# Internet Gateway
+resource "aws_internet_gateway" "radar_gateway" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.vpc_name}-igw"
+  }
+}
+# Public Route Table
+resource "aws_default_route_table" "main" {
+  default_route_table_id = aws_vpc.this.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.radar_gateway.id
+  }
+
+  tags = {
+    Name = "main-rt"
+  }
+}
+
+# Private Route Table
+resource "aws_eip" "nat" {}
+
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = values(aws_subnet.public)[0].id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this.id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  for_each = aws_subnet.private
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private.id
+}
