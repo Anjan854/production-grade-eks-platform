7e76d6cb548815a06b63bb0e6b48e77ecf2eb56f
Ashraf Mahmud <ashraf@filesure.in>
2026-06-24 04:24:18 +0600

    fix: Simplify gitOpsCommit function by removing branch parameter and enhancing error handling

--- jenkins/shared-library/vars/gitOpsCommit.groovy
+++ jenkins/shared-library/vars/gitOpsCommit.groovy
@@ -1,22 +1,28 @@
-def call(String message, String branch = "main") {
+def call(String message) {
 
-    if (!message) {
-        error("Commit message cannot be empty")
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'github-creds',
+            usernameVariable: 'GIT_USER',
+            passwordVariable: 'GIT_TOKEN'
+        )
+    ]) {
+
+        sh """
+            set -e
+
+            git checkout main
+
+            git config user.email "jenkins@local"
+            git config user.name "jenkins"
+
+            git add kubernetes/
+
+            git commit -m "${message}" || echo "No changes to commit"
+
+            git push \
+            https://${GIT_USER}:${GIT_TOKEN}@github.com/Anjan854/production-grade-eks-platform.git \
+            HEAD:main
+        """
     }
-
-    echo "Committing GitOps changes..."
-
-    sh """
-        set -e
-
-        git checkout main
-        git config user.email "jenkins@local"
-        git config user.name "jenkins"
-
-        git add kubernetes/
-
-        git commit -m "${message}" || echo "No changes to commit"
-
-        git push origin ${branch}
-    """
 }
