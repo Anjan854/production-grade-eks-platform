2306615ffe545dcaab0aa069f1c71a393704daaf
Ashraf Mahmud <ashraf@filesure.in>
2026-06-26 01:58:08 +0600

    chore: Refactor registerArgoRepo.groovy for improved readability and formatting

--- jenkins/shared-library/vars/registerArgoRepo.groovy
+++ jenkins/shared-library/vars/registerArgoRepo.groovy
@@ -6,26 +6,26 @@ def call(Map config = [:]) {
             passwordVariable: 'GITHUB_PAT'
         )
     ]) {
+
         sh """
+export PATH=/usr/local/bin:/opt/homebrew/bin:\$PATH
 
-        export PATH=/usr/local/bin:/opt/homebrew/bin:$PATH
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: github-repo
+  namespace: argocd
+  labels:
+    argocd.argoproj.io/secret-type: repository
 
-        cat <<EOF | kubectl apply -f -
-        apiVersion: v1
-        kind: Secret
-        metadata:
-          name: github-repo
-          namespace: argocd
-          labels:
-            argocd.argoproj.io/secret-type: repository
-
-        stringData:
-          type: git
-          url: ${config.repoUrl}
-          username: \$GITHUB_USER
-          password: \$GITHUB_PAT
-        EOF
-        """
+stringData:
+  type: git
+  url: ${config.repoUrl}
+  username: \$GITHUB_USER
+  password: \$GITHUB_PAT
+EOF
+"""
     }
 
     echo 'Repository registered in ArgoCD'
